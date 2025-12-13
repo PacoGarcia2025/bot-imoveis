fad05302dc8a692ed46e3ef3203f156e63e1bb33
Paco Garcia <seu-email@exemplo.com>
2025-12-13 17:15:08 -0300

    Auto Update: 2025-12-13 17:15

---
+++ ligar_robo.bat
@@ -0,0 +1,7 @@
+@echo off
+echo --- INICIANDO O ROBO DE IMOVEIS ---
+echo Nao feche esta janela. Pode apenas minimizar.
+echo ---------------------------------------
+cd /d "C:\bot-imoveis"
+python agendador.py
+pause
